52c575224128770205790e8b6db484e718819de9
javed <javedkhan19950@gmail.com>
2018-01-25 11:26:33 +0530

    remove space to controller file

--- api/controllers/StudentController.js
+++ api/controllers/StudentController.js
@@ -4,7 +4,7 @@
  * @description :: Server-side logic for managing Students
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
- 
+
 module.exports = {
 	DeleteStudentByname : function (req ,res)
 	{
